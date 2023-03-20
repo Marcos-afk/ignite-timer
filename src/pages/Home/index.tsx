79b9e944392261c67504a62345349b34feeaf3b1
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2023-03-20 13:38:27 -0300

    :hammer: improving inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,10 +7,17 @@ export const Home = () => {
       <form action="#">
         <Styled.FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <Styled.TaskInput type="text" id="task" placeholder="Dê um nome ao seu projeto" />
+          <Styled.TaskInput type="text" id="task" placeholder="Dê um nome ao seu projeto" list="task-suggestions" />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </datalist>
 
           <label htmlFor="minutesAmount">Durante</label>
-          <Styled.MinutesAmountInput type="number" id="minutesAmount" placeholder="00" />
+          <Styled.MinutesAmountInput type="number" id="minutesAmount" placeholder="00" step={5} min={5} max={60} />
           <span>Minutos</span>
         </Styled.FormContainer>
 
