a0b73065f9a2f0e7414ca4a46629b88664e0dfa7
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2023-03-20 14:27:39 -0300

    :sparkles: add history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,56 @@
+import * as Styled from './styles';
+
 export const History = () => {
   return (
-    <div>
-      <h1>History</h1>
-    </div>
+    <Styled.HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <Styled.HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Estudar React</td>
+              <td>40 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Styled.Status statusColor="green">Concluído</Styled.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Estudar React</td>
+              <td>40 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Styled.Status statusColor="green">Concluído</Styled.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Estudar React</td>
+              <td>40 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Styled.Status statusColor="yellow">Em andamento</Styled.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Estudar React</td>
+              <td>40 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Styled.Status statusColor="red">Interrompido</Styled.Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Styled.HistoryList>
+    </Styled.HistoryContainer>
   );
 };
